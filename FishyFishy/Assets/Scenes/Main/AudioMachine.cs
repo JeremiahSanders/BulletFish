92716c241967d3ec96b699ecb38e26adc95a4f3f
Jeremiah Sanders <blujai@blujai.com>
2015-09-20 16:49:23 -0500

    Updated audio

--- FishyFishy/Assets/Scenes/Main/AudioMachine.cs
+++ FishyFishy/Assets/Scenes/Main/AudioMachine.cs
@@ -1,15 +1,33 @@
-﻿using UnityEngine;
-using System.Collections;
+﻿using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
 
 public class AudioMachine : MonoBehaviour {
+    private readonly List<AudioClip> QueuedClips = new List<AudioClip>();
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    private void PlaySound()
+    {
+        if (!QueuedClips.Any()) return;
+        var clip = QueuedClips[0];
+        QueuedClips.RemoveAt(0);
+        var src = GetComponent<AudioSource>();
+        src.PlayOneShot(clip);
+    }
+
+    public void PlayVoiceoverClip(AudioClip clip)
+    {
+        if (clip == null) return;
+        QueuedClips.Add(clip);
+    }
+
+    // Use this for initialization
+    private void Start() {}
+
+    // Update is called once per frame
+    private void Update()
+    {
+        var src = GetComponent<AudioSource>();
+        if (src == null || src.isPlaying) return;
+        PlaySound();
+    }
 }
--- FishyFishy/Assets/Scenes/Main/GameMachine.cs
+++ FishyFishy/Assets/Scenes/Main/GameMachine.cs
@@ -39,14 +39,64 @@ public class GameMachine : MonoBehaviour {
     {
         // reset winners each time we fire up the scene
         Winners.Clear();
+        ChangeState(new GameStarting());
+        ChangeState(new GameRunning());
     }
 
+    private void ChangeState(GameState newState)
+    {
+        if (CurrentState != null) {
+            CurrentState.TransitionOut(this);
+        }
+        CurrentState = newState;
+        newState.TransitionIn(this);
+    }
+
+    private GameState CurrentState { get; set; }
+
     // Update is called once per frame
-    private void Update() {}
+    private void Update()
+    {
+        CurrentState.Act(this);
+    }
 
     private abstract class GameState {
-        protected virtual void Act(GameMachine machine) {}
-        protected virtual void TransitionIn(GameMachine machine) {}
-        protected virtual void TransitionOut(GameMachine machine) {}
+        public virtual void Act(GameMachine machine) {}
+        public virtual void TransitionIn(GameMachine machine) {}
+        public virtual void TransitionOut(GameMachine machine) {}
+    }
+
+    private class GameStarting:GameState {
+        private static string[] ClipNames = new[] {
+                                                      IntroClip1,
+                                                      IntroClip2
+                                                  };
+        private const string IntroClip1 = "1a. And they're off!";
+        private const string IntroClip2 = "1b. Lets race boys and gills";
+        public override void TransitionIn(GameMachine machine)
+        {
+            if (machine.AudioSource == null)return;
+            int clipIndex = Random.Range(0, (ClipNames.Length - 1));
+            string clipName = ClipNames[clipIndex];
+            Debug.Log("Playing "+clipName);
+            var clip = new AudioClip {name = clipName};
+            machine.AudioSource.PlayVoiceoverClip(clip);
+        }
+    }
+
+    private class GameRunning : GameState {
+        private static string[] RandomClips = new[] {
+                                                        "back to school"
+                                                    };
+        public override void Act(GameMachine machine)
+        {
+            if (machine.AudioSource == null) return;
+            var shouldPlaySound = Random.value > 0.97;
+            if (!shouldPlaySound)return;
+            int clipIndex = Random.Range(0, (RandomClips.Length - 1));
+            string clipName = RandomClips[clipIndex];
+            var clip = new AudioClip { name = clipName };
+            machine.AudioSource.PlayVoiceoverClip(clip);
+        }
     }
 }
