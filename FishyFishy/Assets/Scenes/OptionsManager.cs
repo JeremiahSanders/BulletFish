2eda3e54dacb4dd64e9bb0f707a5ae35b0db7270
Jeremiah Sanders <blujai@blujai.com>
2015-09-20 15:39:45 -0500

    Fixed volume slider

--- FishyFishy/Assets/Scenes/OptionsManager.cs
+++ FishyFishy/Assets/Scenes/OptionsManager.cs
@@ -3,15 +3,21 @@ using UnityEngine.UI;
 
 public class OptionsManager : MonoBehaviour {
     public Slider VolumeSlider;
-
+    private bool screenLoaded;
     public void OnVolumeSliderChanged()
     {
+        if (!screenLoaded) return;
         if (VolumeSlider == null) return;
         AudioListener.volume = VolumeSlider.value;
     }
 
     // Use this for initialization
-    private void Start() {}
+    private void Start()
+    {
+        if (VolumeSlider == null) return;
+        VolumeSlider.value = AudioListener.volume;
+        screenLoaded = true;
+    }
 
     // Update is called once per frame
     private void Update() {}
