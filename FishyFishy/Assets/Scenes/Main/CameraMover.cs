194efd55ee89e4320095e2c691278d3a05262991
Jeremiah Sanders <blujai@blujai.com>
2015-09-23 21:49:57 -0500

    Updated main scene to prep for camera movement scripting

---
+++ FishyFishy/Assets/Scenes/Main/CameraMover.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraMover : MonoBehaviour {
+    public GameObject CutsceneCamera;
+    public GameObject MainCamera;
+    public GameObject HudWrapper;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+    private void DoIntroSweep()
+    {
+        CutsceneCamera.SetActive(true);
+        MainCamera.SetActive(false);
+
+
+    }
+
+    enum CameraStates {
+        IntroSweep,
+        Game
+    }
+}
